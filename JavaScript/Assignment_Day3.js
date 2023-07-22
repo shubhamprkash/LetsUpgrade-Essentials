a89c0cfc812da40f3f560f9e5b849c0df94004f9
Shubham Prakash <56962038+shubhamprkash@users.noreply.github.com>
2023-07-22 20:41:30 +0530

    Assignment_Day3.js
    Day 3 assignment :
    
    Write a JavaScript function called "calculateArea" that takes two parameters: "length" and "width". The function should calculate and return the area of a rectangle using the given length and width.

---
+++ JavaScript/Assignment_Day3.js
@@ -0,0 +1,11 @@
+// Write a JavaScript function called "calculateArea" that takes two parameters: "length" and "width". 
+// The function should calculate and return the area of a rectangle using the given length and width.
+
+function calculateArea(length, width){   
+  return length * width;
+}  // function to calculate area based on length and width
+
+let len = 5;      //sample inputs
+let wid = 10;
+
+console.log(calculateArea(len, wid));  // 50
